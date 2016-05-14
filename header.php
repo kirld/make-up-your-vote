714aa9d23155a5480b044dfb121faec6d3c27ca5
mackelele <makiraicevic@gmail.com>
2016-05-13 17:28:36 -0700

    Mack-Commit01
    Included header.php, home.php file with main.scss.
    Home page is responsive but not finished yet.

---
+++ header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <!-- Oswald Goole Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+    <!-- Roboto Goole Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,700,900,300' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="css/app.css">
+    	<!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+  </head>
+  <body>
