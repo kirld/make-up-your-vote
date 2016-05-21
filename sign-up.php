f103495220ed096a58d78b262ecbc44b70966f4a
kirld <kiraderksen@hotmail.ca>
2016-05-20 20:22:18 -0700

    sign up updated
    Sign up updated without session in header

--- sign-up.php
+++ sign-up.php
@@ -1,5 +1,23 @@
-<?php include 'header.php' ?>
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <!-- Oswald Goole Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+    <!-- Roboto Goole Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,700,900,300' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="css/app.css">
+        <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+  </head>
+  <body>
 <?php include 'navigation.php' ?>  
+<?php include 'newsletter.php' ?>
 <div class="row">
     
     <div class="small-12 medium-push-3 medium-6 columns">
@@ -45,11 +63,5 @@
     
 </div>
 
-    <script src="bower_components/jquery/dist/jquery.js"></script>
-<script src="bower_components/what-input/what-input.js"></script>
-<script src="bower_components/foundation-sites/dist/foundation.js"></script>
-<script src="js/app.js"></script>
-<script src="js/main.js"></script>
-</body>
-</html> 
+<?php include 'footer.php' ?>
 
